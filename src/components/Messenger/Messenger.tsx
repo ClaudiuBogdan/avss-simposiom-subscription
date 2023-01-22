0838a9f004268eaae0955d5ee94ab95b86586bb6
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-01-22 17:13:41 +0200

    fix: generate unique id for messenger component

--- src/components/Messenger/Messenger.tsx
+++ src/components/Messenger/Messenger.tsx
@@ -1,42 +1,40 @@
 import { webConfig } from "@/config/web";
-import { FC, useEffect } from "react";
+import Script from "next/script";
+import { FC, useEffect, useId } from "react";
 
 export const Messenger: FC = () => {
-  useEffect(() => {
-    if(!webConfig.facebook.pageId) return;
-    
-    var chatbox = document.getElementById("fb-customer-chat");
-    if (!chatbox) return;
-    chatbox.setAttribute("page_id", webConfig.facebook.pageId);
-    chatbox.setAttribute("attribution", "biz_inbox");
+  const facebookPageId = webConfig.facebook.pageId;
+ const uuid = useId();
+ const fbScriptId = `fb-script-${uuid}`;
 
-    if (!window) return;
-
-    // @ts-ignore
-    window.fbAsyncInit = function () {
-      // @ts-ignore
-      FB.init({
-        xfbml: true,
-        version: "v15.0",
-      });
-    };
-
-    (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-
-      js = d.createElement(s) as HTMLScriptElement;
-      js.id = id;
-      js.src = "https://connect.facebook.net/ro_RO/sdk/xfbml.customerchat.js";
-      fjs.parentNode!.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
-  }, []);
   return (
     <>
       <div id="fb-root"></div>
 
       <div id="fb-customer-chat" className="fb-customerchat"></div>
+      <Script strategy="lazyOnload" id="fb-script-container">
+        {`
+    
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "${facebookPageId}}");
+            chatbox.setAttribute("attribution", "biz_inbox");
+
+            window.fbAsyncInit = function() {
+            FB.init({
+                xfbml            : true,
+                version          : 'v15.0'
+            });
+            };
+
+            (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/ro_RO/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', '${fbScriptId}'));
+            `}
+      </Script>
     </>
   );
 };
