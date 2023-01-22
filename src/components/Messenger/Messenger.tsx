4b79910824eecdc01f28b4b44e7fa31bfeebe9ca
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-01-22 13:37:33 +0200

    fix: messenger lint errors

--- src/components/Messenger/Messenger.tsx
+++ src/components/Messenger/Messenger.tsx
@@ -2,6 +2,7 @@ import { FC, useEffect } from "react";
 
 export const Messenger: FC = () => {
   useEffect(() => {
+    // @ts-ignore
     window.fbAsyncInit = function () {
       //@ts-ignore
       FB.init({
@@ -16,14 +17,18 @@ export const Messenger: FC = () => {
       if (d.getElementById(id)) return;
       js = d.createElement(s);
       js.id = id;
+    // @ts-ignore
       js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+    // @ts-ignore
       fjs.parentNode.insertBefore(js, fjs);
     })(document, "script", "facebook-jssdk");
   });
 
   useEffect(() => {
     var chatbox = document.getElementById("fb-customer-chat");
+    // @ts-ignore
     chatbox.setAttribute("page_id", "100083874741432");
+    // @ts-ignore
     chatbox.setAttribute("attribution", "biz_inbox");
   }, []);
 
