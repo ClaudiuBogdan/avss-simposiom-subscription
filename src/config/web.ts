437f14369ce23eaf8654a361f0dcddd46655e414
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-01-22 16:59:07 +0200

    fix: load web env var

--- src/config/web.ts
+++ src/config/web.ts
@@ -2,6 +2,6 @@ import { loadEnv } from "./utils";
 
 export const webConfig = {
     facebook: {
-        pageId: loadEnv("NEXT_PUBLIC_FACEBOOK_PAGE_ID"),
+        pageId: loadEnv("NEXT_PUBLIC_FACEBOOK_PAGE_ID", ""),
     }
 }
